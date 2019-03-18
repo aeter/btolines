573079d912f0dcbb07b58b81c10218fb5013cdff
A. Nackov <anackov@gmail.com>
2019-03-18 20:53:16 +0200

    Draw simple line

--- boot.asm
+++ boot.asm
@@ -1,3 +1,5 @@
+%define sleep hlt
+
 SETUP:
     bits 16
     ; BIOS jumps to 0x7C00 if the 511th/512th bytes contain the boot signature
@@ -6,10 +8,31 @@ SETUP:
 
     .clear_registers:
         xor ax, ax
+        xor bx, bx
 
 SET_VIDEO_MODE_AND_CLEAR_SCREEN:
     mov ax, 13h  ; https://en.wikipedia.org/wiki/Mode_13h - (320x200x256 colors)
     int 10h ; https://en.wikipedia.org/wiki/INT_10H
+
+START:
+draw_line:
+    inc cx ; x
+    inc dx ; y
+    cmp cx, 50 ; x
+    je return
+    call draw_pixel
+    sleep
+    jmp draw_line
+
+; ah = 0Ch, al = color, bh = page, cx = x, dx = y
+draw_pixel:
+    mov ah, 0Ch  ; function OCh = write pixel
+    mov al, 0x3 ; green color
+    mov bh, 1 ; page number
+    int 10h
+    ret
+
+return: ret
 
 BOOT_SIGNATURE:
     times 510 - ($-$$) db 0 ; pad with zeroes up to 510th byte
